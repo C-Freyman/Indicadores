d6844b5bf0d6d1b8e2f568e4015fffd0e2827993
angeles garcia herrera <agarcia@lfoasociados.com.mx>
2024-06-18 15:00:32 -0600

    valida borrado

--- IndicadoresFreyman/asignaIndicadores.aspx.designer.cs
+++ IndicadoresFreyman/asignaIndicadores.aspx.designer.cs
@@ -114,6 +114,15 @@ namespace IndicadoresFreyman
         protected global::System.Web.UI.WebControls.HiddenField hdnEmpleado;
 
         /// <summary>
+        /// Control hdnEmpleadoId.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hdnEmpleadoId;
+
+        /// <summary>
         /// Control hdnCorreo.
         /// </summary>
         /// <remarks>
