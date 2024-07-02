1c208797043c0c97155edbe7629c5c9139b45dc7
dfernandez <dfernandez@PC-DFERNANDEZ>
2024-07-02 12:47:27 -0600

    .

--- IndicadoresFreyman/Indicadores/HistoricoIndicadoresM.aspx.cs
+++ IndicadoresFreyman/Indicadores/HistoricoIndicadoresM.aspx.cs
@@ -53,7 +53,7 @@ namespace IndicadoresFreyman.Indicadores
         {
             var i = Session["Log"];
             //Puestos= 0-colaborador////1-gerente/////2-Contralor
-            if (i.ToString() == "42")//Contralor
+            if (i.ToString() == "42" || i.ToString() == "132")//Contralor
             {
                 Session["puesto"] = "2";
                 Literal1.Text = "<script>document.getElementById('divColaboradores').style.display = 'inline-block';</script>";
